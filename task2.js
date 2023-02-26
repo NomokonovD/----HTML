27a4b619281602c42f6b20f53dbfad2a9136a239
Cybercriminal <danila.nomokonov@mail.ru>
2023-02-26 20:15:18 +0700

    Задание 2 + корректировки по 1 заданию

--- task2.js
+++ task2.js
@@ -1,10 +1,23 @@
 let checkBtn=false;
 let flag=0;
-let arr=[];
+let arrLNK=[];
+let arrDPS=[];
 
 function clickButt1(){
-    if(checkBtn){
-        NewGame();
+    if(checkBtn){ //если нажата другая кнопка (ДПС)
+        let btn2=document.querySelector("#btn2");// красим в синий вторую , которая была нажата до этого
+        btn2.style.backgroundColor='#31b5fc';
+
+        let btn1=document.querySelector("#btn1"); // красим в оранж первую и выводим словами из массива 
+        btn1.style.backgroundColor='#FF9900';
+        document.getElementById("inputWord").innerHTML="Л Н К";
+        let str="";
+        for(let i of arrLNK){
+            str+=i+", ";
+        }
+        document.getElementById("ListW").innerHTML=str;
+        flag=1;
+
     }
     else{
         let btn1=document.querySelector("#btn1");
@@ -17,13 +30,26 @@ function clickButt1(){
 
 function clickButt2(){
     if(checkBtn){
-        NewGame();
+        let btn1=document.querySelector("#btn1");// красим в синий первую , которая была нажата до этого
+        btn1.style.backgroundColor='#31b5fc';
+
+        let btn2=document.querySelector("#btn2"); // красим в оранж вторую и выводим словами из массива 
+        btn2.style.backgroundColor='#FF9900';
+        document.getElementById("inputWord").innerHTML="Д П С";
+        let str="";
+        for(let i of arrDPS){
+            str+=i+", ";
+        }
+        document.getElementById("ListW").innerHTML=str;
+        flag=0;
     }
     else{
+
         let btn2=document.querySelector("#btn2");
         btn2.style.backgroundColor='#FF9900';
         document.getElementById("inputWord").innerHTML="Д П С";
         checkBtn=true;
+
     }
 }
 
@@ -34,10 +60,10 @@ function Game(){
 
     if(flag==1){ //если нажата левая кнопка с буквами 
 
-        if(checking_letters(word) && word!="" && arr.indexOf(word.toUpperCase())==-1 && word.length >=5){
-            arr.push(word.toUpperCase());
+        if(checking_letters(word) && word!="" && arrLNK.indexOf(word.toUpperCase())==-1 && word.length >=5){
+            arrLNK.push(word.toUpperCase());
 
-            for(let i of arr){
+            for(let i of arrLNK){
                 str+=i+", ";
             }
             document.getElementById("ListW").innerHTML=str;
@@ -46,8 +72,21 @@ function Game(){
         else{
             document.getElementById("Err").innerHTML="Данное слово не подходит";
         }
+    }
+    else{
 
+        if(checking_letters(word) && word!="" && arrDPS.indexOf(word.toUpperCase())==-1 && word.length >=5){
+            arrDPS.push(word.toUpperCase());
 
+            for(let i of arrDPS){
+                str+=i+", ";
+            }
+            document.getElementById("ListW").innerHTML=str;
+            document.getElementById("Err").innerHTML="";
+        }
+        else{
+            document.getElementById("Err").innerHTML="Данное слово не подходит";
+        }
     }
 
     document.getElementById("input").value="";
